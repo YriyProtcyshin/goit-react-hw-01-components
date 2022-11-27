edcaffd25a5c7a3c94f870e2c1e563bc95cf21bf
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-11-27 22:13:06 +0200

    task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,16 @@ import userJson from 'user.json';
 export const App = () => {
   console.log(userJson);
   return (
-    <Profile
-      username={userJson.username}
-      tag={userJson.tag}
-      location={userJson.location}
-      avatar={userJson.avatar}
-      followers={userJson.stats.followers}
-      views={userJson.stats.views}
-      likes={userJson.stats.likes}
-    />
+    <>
+      <Profile
+        username={userJson.username}
+        tag={userJson.tag}
+        location={userJson.location}
+        avatar={userJson.avatar}
+        followers={userJson.stats.followers}
+        views={userJson.stats.views}
+        likes={userJson.stats.likes}
+      />
+    </>
   );
 };
